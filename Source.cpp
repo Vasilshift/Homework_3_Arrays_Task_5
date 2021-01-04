8769741fa4798d7b08844bddeb819c981ce157ac
Vasilshift <Vasilshift@github.com>
2021-01-04 16:12:36 +0500

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdlib>
+#include <array>
+#include<iterator>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+	int i;
+	const int n = 5;
+	int arr1[n] = { 1, 2, 3, 4, 5 };
+	int arr2[n] = { 5, 6, 7, 8, 9 };
+
+	int size = sizeof(arr1) / sizeof(arr1[0]);
+	
+	for (int i = 0; i < size; i++) {
+
+		cout << arr1[i] + arr2[i] << " ";
+
+	}
+
+
+
+	return 0;
+}
